0b0cc3463baa9a2f3d634ef378fd516d0bfe8e6f
Elar Saks <elarsaks@gmail.com>
2024-12-06 16:16:49 +0200

    Improve UI

--- app.js
+++ app.js
@@ -3,18 +3,24 @@
 // ============================================================================
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("gameCanvas");
+  const canvasHeight = window.innerHeight * 0.85;
+  const canvasWidth = window.innerWidth;
+  canvas.height = canvasHeight;
+  canvas.width = canvasWidth;
+
+  const cellHeight = 10;
+  const rows = Math.floor(canvasHeight / cellHeight) + 1;
+  const cols = Math.floor(canvasWidth / cellHeight) + 1;
 
   // Use a builder to create the initial board
   const board = new BoardBuilder()
-    .setRows(25)
-    .setCols(25)
-    .setInitializer(() => {
-      return CellFactory.createDeadCell();
-    })
+    .setRows(rows)
+    .setCols(cols)
+    .setInitializer(() => CellFactory.createDeadCell())
     .build();
 
   // Create an adapter for rendering
-  const adapter = new CanvasAdapter(canvas, 20);
+  const adapter = new CanvasAdapter(canvas, 10);
 
   // Add an observer for the board to redraw when updated
   const displayObserver = new DisplayObserver(adapter);
@@ -23,7 +29,9 @@ document.addEventListener("DOMContentLoaded", () => {
   // Create or get the singleton game instance
   const game = new Game();
   game.init(board);
-  board.notifyObservers(); // initial draw
+  // TODO: Replace initial draw with randomize
+  // board.notifyObservers(); // initial draw
+  game.randomize();
 
   // Controls
   document.getElementById("startBtn").addEventListener("click", () => {
